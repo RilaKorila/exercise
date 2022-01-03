c3ac4d48f2aa7683227686c5fa5e387048672617
RilaKorila <murakami.ayana3@gmail.com>
2022-01-03 23:39:59 +0900

    baba

---
+++ 2021Dec/baba.js
@@ -0,0 +1,86 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    [h, w, n] = lines[0].split(" ")
+    h = Number(h)
+    w = Number(w)
+    n = Number(n)
+
+    var field = []
+    // 外枠は . にする
+    field[0] = ".".repeat(w+2).split("")
+    for(var i=1; i<h+1; i++){
+        field[i] = ["."].concat(lines[i].split(" "), ["."])
+    }
+    field[h+1] = ".".repeat(w+2).split("")
+
+    var check = [];
+    var counter = 0;
+    for(i=h+1; i<h+n+1; i++){
+        check[counter] = lines[i].split(" ")
+        counter+=1
+    }
+
+    var a1, b1, a2, b2, start, end;
+    for(i=0; i<counter; i++){
+        a1 = Number(check[i][0])
+        b1 = Number(check[i][1])
+        a2 = Number(check[i][2])
+        b2 = Number(check[i][3])
+
+        start = `(${a1}, ${b1})`
+        end = `(${a2}, ${b2})`
+
+        // DFS(push/pop)
+        var stack = [];
+        var visited = [];
+
+        // startの座標を格納
+        stack.push([a1, b1])
+        console.log(stack)
+
+        var now=""
+        while(stack.length > 0){
+            [x, y] = stack.pop()
+            now = `(${x}, ${y})`
+            visited.push(now)
+            console.log(`(${now})は訪れた`)
+            console.log(`visited: ${visited}`)
+            if(([x,y+1] === [a2,b2]) || ([x,y-1] === [a2,b2]) || ([x+1,y] === [a2,b2]) || ([x-1,y] === [a2,b2])){ // 外側にたどり着いた
+                IsAchieved = true
+                break;
+            }
+
+            // . のところのみ移動可能
+            if((visited.indexOf([x,y-1]) === -1) && (field[x][y-1] === ".")){
+                stack.push([x, y-1])
+            }
+            if((visited.indexOf([x,y+1]) === -1) && (field[x][y+1] === ".")){
+                stack.push([x, y+1])
+            }
+            if((visited.indexOf([x-1,y]) === -1) && (field[x-1][y] === ".")){
+                stack.push([x-1, y])
+            }
+            if((visited.indexOf([x+1,y]) === -1) && (field[x+1][y] === ".")){
+                stack.push([x+1,y])
+            }
+        }
+
+        if(IsAchieved){
+            console.log("yes")
+        }
+        else{
+            console.log("no")
+        }
+    }
+});
