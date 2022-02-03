c511133003d04857a3c9a6885488b0b715f7183e
RilaKorila <murakami.ayana3@gmail.com>
2022-02-04 07:11:12 +0900

    fix

--- BinarySerch/subsequence.py
+++ BinarySerch/subsequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
-        # n: length of original sequence, k: length of removable list   
+        # n: how long it takes to search sub sequence, k: length of removable list   
         # brute force: O(nk)
         # binary search: 0(n log(k))
         
@@ -14,23 +14,25 @@ class Solution:
                 # 2. if the character in s is different from character in subsequence, 
                 # we have to check next character is s
                 if i1 in removed or s[i1] != subseq[i2]:
+                    # only i1 will be shifted to next position
                     i1 += 1
+                    # continue next iteration loop
                     continue
-                # if we find part of subseqt
+                # if we find part of subseq, both pointer will be shifted
                 i1 += 1
                 i2 += 1
             
             return i2 == len(subseq)
             # ↑ in the situation, i2 can reach at the end of the subsequence
         
-        removed = set()
-        res = 0
+        res = 0 
+        # we will do bin search on removable
         l, r = 0, len(removable)-1
         
         while l<=r:
             m = (l+r)//2
             
-            # position we already removed from 
+            # the position we already removed from 
             removed = set(removable[:m+1])
             if isSubsequence(s, p):
                 # we have to return the number of removable characters, not index
@@ -39,6 +41,6 @@ class Solution:
             else:
                 r = m-1
         return res
-        
-        
-                    
+
+# ---
+# Why binary search is efficient in this problem?
