b36e694f1a785a313853e26926b23dfc9801beb0
RilaKorila <murakami.ayana3@gmail.com>
2022-02-04 11:30:09 +0900

    koko

---
+++ BinarySerch/koko.py
@@ -0,0 +1,35 @@
+# koko eats all bananas from all piles until the guardian comes back
+# len(piles) < h: 
+# if the number of piles is more than h, koko cannot eat all bananas even if she eats tons of bananas per hour
+
+# 1. brute force: O(max(piles)・piles)
+# we check k from 1 to max value in piles list
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        def canEatAllBananas(piles, k, h):
+            time = 0
+            for pile in piles:
+                if pile%k == 0:
+                    time += (pile // k)
+                else:
+                    time += (pile // k) + 1
+                
+            return time <= h
+        
+        
+        l, r = 1, max(piles)
+        
+        k = -1
+        while l<=r:
+            m = (l+r)//2
+            if canEatAllBananas(piles, m ,h):
+                k = m
+                r = m-1
+            else:
+                l = m+1
+        
+        return k        
+        
+# I wrote all code by myself ^^
