2c8900e7ecbe69a1715ef65cb589bd4c64d52cbd
RilaKorila <murakami.ayana3@gmail.com>
2022-02-07 08:34:46 +0900

    min

--- StackProblems/min_stack.py
+++ StackProblems/min_stack.py
@@ -1,19 +1,29 @@
 class MinStack:
     # retrieve the min element in constant time
+    # Hint: Consider each node in the stack having a minimum value
 
     def __init__(self):
+        self.stack = []
+        self.minStack = []
         
 
     def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
         
 
     def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
         
 
     def top(self) -> int:
+        return self.stack[-1]
         
 
     def getMin(self) -> int:
+        return self.minStack[-1]
         
 
 
