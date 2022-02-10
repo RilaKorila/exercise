86eb95f0eadafa9fdc55c0ccc5dcb5f354fc582f
RilaKorila <murakami.ayana3@gmail.com>
2022-02-10 22:15:13 +0900

    wip

---
+++ DynamicProgramming/stairs.py
@@ -0,0 +1,12 @@
+# we can climb 1 or 2 steps each time
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # DP: store memory, and skip the same execution
+        one, two = 0, 1
+        
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+        
+        return one
