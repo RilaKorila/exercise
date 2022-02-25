da85aba8ff4e5575328417222a6669f97454e0f8
RilaKorila <murakami.ayana3@gmail.com>
2022-02-25 23:25:04 +0900

    fix

--- SlidingWindow/subArray.py
+++ SlidingWindow/subArray.py
@@ -12,4 +12,23 @@ class Solution:
                 r += 1
 
         return res if res != len(nums)+1 else 0
+    
+    def NeetCode(self, target: int, nums: List[int]) -> int:
+        l, total = 0, 0
+        res = float("inf")
+        
+        for r in range(len(nums)):
+            total += nums[r]
+            
+            # if condition(total >= target) continues, we continue to increase left pointer 
+            while total >= target:
+                # res is size of the window
+                res = min(res, r-l+1)
+                # update the window
+                total -= nums[l]
+                l += 1
+                
+        
+        
+        return 0 if res == float("inf") else res
                 
