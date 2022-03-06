9945837f5261bc662d3b32b096ae771af6bd54cb
RilaKorila <murakami.ayana3@gmail.com>
2022-03-07 07:44:24 +0900

    stack

--- Trees/traverse.py
+++ Trees/traverse.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from typing import List
+
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         # base case
@@ -15,3 +18,30 @@ class Solution:
     # inorder: left->root->right
     # preorder: root->left->right
     # postorder: left->right->root
+    
+    def inorderWithoutRecursion(self, root: Optopnal[TreeNode])->List[int]:
+        # solusion using stack: O(n)
+        s = []
+        cur = root
+        res = []
+        
+        # traverse left side node until cur is NULL
+        while True:
+            if cur:
+                s.append(cur)
+                cur = cur.left
+            
+            # if current root is NULL and stack is not empty
+            if not cur and s:
+                cur = s.pop()
+                res.append(cur.val)
+                # ここをpopしたnodeのrightにしたい
+                cur = cur.right
+            
+            # if current node is NULL and stack is empty
+            elif not cur and not s:
+                break
+                
+        return res
+
+    
