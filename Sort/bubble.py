3d061cd70146fec25b33ffaa901df7c10662998b
RilaKorila <murakami.ayana3@gmail.com>
2022-03-23 23:57:02 +0900

    fic

--- Sort/bubble.py
+++ Sort/bubble.py
@@ -3,8 +3,6 @@ class Sort:
         
         for i in range(len(nums)):
             for j in range(len(nums)-1-i):
+                # if nums[j] is larger than nums[j+1], swap them
                 if nums[j] > nums[j+1]:
-                    tmp = nums[j]
-                    nums[j] = nums[j+1]
-                    nums[j+1] = tmp
-        
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
